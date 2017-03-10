aecda53edf1af7f38b5104cedec2995f633e1635
guillemportella <guillemportella@gmail.com>
2017-03-10 16:17:48 +0000

    Partial implementation vanNoort. E calculation should work.

--- nuc_vannoort.hpp
+++ nuc_vannoort.hpp
@@ -17,8 +17,8 @@
 #define PI 3.14159265
 
 template <typename tt1, typename tt2, typename tt3, typename tt4>
-Eigen::VectorXf return_dinuc_weight(tt1 w, tt2 b, tt3 p, tt4 div = 1) {
-  Eigen::VectorXf vect(w);
+Eigen::VectorXd return_dinuc_weight(tt1 w, tt2 b, tt3 p, tt4 div = 1) {
+  Eigen::VectorXd vect(w);
   for (unsigned i = 0; i < w; ++i) {
     vect(i) = 0.25 + b * sin(2 * PI * i / p) / div;
   }
@@ -26,82 +26,106 @@ Eigen::VectorXf return_dinuc_weight(tt1 w, tt2 b, tt3 p, tt4 div = 1) {
   return vect;
 }
 
-template <typename tt1, typename tt2>
-std::vector<std::vector<Eigen::VectorXf>> getweights(tt1 seqs, tt2 cond) {
+template <typename tt1>
+std::vector<std::vector<Eigen::VectorXd>> getweights(tt1 cond) {
   // There might not be an actuall reason to use Eigen here
   // but since I need it anyway for nuc_elastic, I use it
 
   unsigned window = 74;
   float period = 10.1;
   float amp = 0.2;
-  Eigen::VectorXf AA = return_dinuc_weight(window, amp, period, 1);
-  Eigen::VectorXf AC = return_dinuc_weight(window, -amp, period, 3);
-  Eigen::VectorXf AG = AC;
-  Eigen::VectorXf AT = AC;
+  Eigen::VectorXd AA = return_dinuc_weight(window, amp, period, 1);
+  Eigen::VectorXd AC = return_dinuc_weight(window, -amp, period, 3);
+  Eigen::VectorXd AG = AC;
+  Eigen::VectorXd AT = AC;
 
   // the one below is actually 0.25 for all, decide if you want to initialize
   // to that, or compute 0*sinus to have a more compact way. I hope the
   // compiler catches that b=0
-  Eigen::VectorXf CA = return_dinuc_weight(window, 0, period, 1);
-  Eigen::VectorXf CC = CA;
-  Eigen::VectorXf CG = CA;
-  Eigen::VectorXf CT = CA;
+  Eigen::VectorXd CA = return_dinuc_weight(window, 0, period, 1);
+  Eigen::VectorXd CC = CA;
+  Eigen::VectorXd CG = CA;
+  Eigen::VectorXd CT = CA;
 
-  Eigen::VectorXf GA = return_dinuc_weight(window, amp, period, 3);
-  Eigen::VectorXf GC = return_dinuc_weight(window, -amp, period, 1);
-  Eigen::VectorXf GG = GA;
-  Eigen::VectorXf GT = GA;
+  Eigen::VectorXd GA = return_dinuc_weight(window, amp, period, 3);
+  Eigen::VectorXd GC = return_dinuc_weight(window, -amp, period, 1);
+  Eigen::VectorXd GG = GA;
+  Eigen::VectorXd GT = GA;
 
-  Eigen::VectorXf TA = return_dinuc_weight(window, amp, period, 1);
-  Eigen::VectorXf TC = return_dinuc_weight(window, -amp, period, 1);
-  Eigen::VectorXf TG = TC;
-  Eigen::VectorXf TT = TA;
+  Eigen::VectorXd TA = return_dinuc_weight(window, amp, period, 1);
+  Eigen::VectorXd TC = return_dinuc_weight(window, -amp, period, 1);
+  Eigen::VectorXd TG = TC;
+  Eigen::VectorXd TT = TA;
 
-  std::vector<Eigen::VectorXf> v_As = {AA, AC, AG, AT};
-  std::vector<Eigen::VectorXf> v_Cs = {CA, CC, CG, CT};
-  std::vector<Eigen::VectorXf> v_Gs = {GA, GC, GG, GT};
-  std::vector<Eigen::VectorXf> v_Ts = {TA, TC, TG, TT};
-  std::vector<std::vector<Eigen::VectorXf>> weights = {v_As, v_Cs, v_Gs, v_Ts};
+  std::vector<Eigen::VectorXd> v_As = {AA, AC, AG, AT};
+  std::vector<Eigen::VectorXd> v_Cs = {CA, CC, CG, CT};
+  std::vector<Eigen::VectorXd> v_Gs = {GA, GC, GG, GT};
+  std::vector<Eigen::VectorXd> v_Ts = {TA, TC, TG, TT};
+  std::vector<std::vector<Eigen::VectorXd>> weights = {v_As, v_Cs, v_Gs, v_Ts};
 
   return weights;
 }
 
-/*
-template <typename tt1> int base2index(tt1 b) {
-  switch (b) {
-  case 'A':
-    return 0;
-  case 'C':
-    return 1;
-  case 'G':
-    return 2;
-  case 'T':
-    return 3;
-  default:
-    std::cout << "Invalid nucleotide" << std::endl;
-    exit(0);
-  }
-}
-*/
-
-template <typename tt1, typename tt2> void do_vannoort(tt1 seq, tt2 cond) {
+template <typename tt1, typename tt2>
+Eigen::VectorXd do_vannoort(tt1 seq, tt2 cond) {
   unsigned window = 74;
-  for (unsigned i = 0; i < length(seq) - window; ++i) {
-    float p_s_f = 1.0;
-    float p_s_r = 1.0;
-    for (unsigned s = 0; s < window; ++s) {
-      unsigned ii = (unsigned)ordValue(seq[i + s]);
-      unsigned jj = (unsigned)ordValue(seq[i + s + 1]);
-      // Segfault here?? Check logic
-      unsigned ri = 3 - (unsigned)ordValue(seq[i + window - s]);
-      unsigned rj = 3 - (unsigned)ordValue(seq[i + window - s + 1]);
-    }
+  // first one is special due to pbc and weird averaging done in original script
+  // I want to avoid using anf if inside the loop, so I just make it explicit
+  // notice how the two strands are slanted // offset by one ... no idea why
+  std::vector<std::vector<Eigen::VectorXd>> weights = getweights(cond);
+  Eigen::VectorXd pf(length(seq) - window);
+  Eigen::VectorXd pr(length(seq) - window);
+  double ps_f = 1.0;
+  double ps_r = 1.0;
+  for (unsigned s = 0; s < window; ++s) {
+    unsigned ii = (unsigned)ordValue(seq[length(seq) - 1]);
+    unsigned jj = (unsigned)ordValue(seq[s]);
+    ps_f *= weights[ii][jj](s);
+    unsigned ri = 3 - (unsigned)ordValue(seq[window - s]);
+    unsigned rj = 3 - (unsigned)ordValue(seq[window - s - 1]);
+    ps_r *= weights[ri][rj](s);
   }
+  pf(0) = ps_f;
+  pr(0) = ps_r;
+
+  // now proceed from 1 onward
+  for (unsigned i = 1; i < length(seq) - window; ++i) {
+    double ps_f = 1.0;
+    double ps_r = 1.0;
+    for (unsigned s = 0; s < window; ++s) {
+      unsigned ii = (unsigned)ordValue(seq[i + s - 1]);
+      unsigned jj = (unsigned)ordValue(seq[i + s]);
+      ps_f *= weights[ii][jj](s);
+      std::cout << ps_f << std::endl;
+      unsigned ri = 3 - (unsigned)ordValue(seq[i + window - s]);
+      unsigned rj = 3 - (unsigned)ordValue(seq[i + window - s - 1]);
+      ps_r *= weights[ri][rj](s);
+    }
+    pf(i) = ps_f;
+    pr(i) = ps_r;
+  }
+  // multiply all by 4^window
+  for (unsigned i = 0; i < pf.size(); ++i) {
+    pf(i) *= std::pow(4, window);
+    pr(i) *= std::pow(4, window);
+  }
+  // circular shift pr
+  for (unsigned i = 0; i < pr.size() - 1; ++i) {
+    auto p = pr(i);
+    pr(i) = pr(i + 1);
+    pr(i + 1) = p;
+  }
+  // Average both strands
+
+  Eigen::VectorXd E(length(seq) - window);
+  for (unsigned i = 0; i < pr.size(); ++i) {
+    E(i) = pr(i) * log(pr(i)) + pf(i) * log(pf(i)) / (pr(i) + pf(i));
+  }
+  return E;
 }
 
 template <typename tt1, typename tt2> void do_all_vannoort(tt1 seqs, tt2 cond) {
 
-  std::vector<std::vector<Eigen::VectorXf>> weights = getweights(seqs, cond);
   //#pragma omp parallel for
   for (unsigned i = 0; i < length(seqs); ++i) {
     if (length(seqs[i]) >= NUC_LEN) {
