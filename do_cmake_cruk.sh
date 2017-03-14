c24a6b625feaeec81a4e0b01798fd3d82a94caf9
Guillem Portella <portel01@clust1-headnode-1.cri.camres.org>
2017-03-14 21:47:05 +0000

    Added cmakelist_cruk, etc..

---
+++ do_cmake_cruk.sh
@@ -0,0 +1,3 @@
+cp CMakeLists_cruk.txt CMakeLists.txt 
+cmake3 . -DCMAKE_C_COMPILER=/home/portel01/programs/compilers/gcc-6.3/bin/gcc -DCMAKE_CXX_COMPILER=/home/portel01/programs/compilers/gcc-6.3/bin/g++ 
+
