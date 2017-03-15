66af46e3b5b27a15354f419ed38bd7577116317f
guillemportella <guillemportella@gmail.com>
2017-03-15 11:25:59 +0000

    Added support for bz2 input

--- do_cmake_mbook.sh
+++ do_cmake_mbook.sh
@@ -1,1 +1,2 @@
+cp CMakeLists_mbook.txt CMakeLists.txt
  cmake . -DCMAKE_C_COMPILER=/opt/local/bin/clang-mp-3.9 -DCMAKE_CXX_COMPILER=/opt/local/bin/clang++-mp-3.9 -DOpenMP_C_FLAGS=/opt/local/lib/libomp -DOpenMP_CXX_FLAGS=/opt/local/lib/libomp -DSEQAN_INCLUDE_PATH=/Users/guillem/work/soft/seqan-library-2.2.0/include
